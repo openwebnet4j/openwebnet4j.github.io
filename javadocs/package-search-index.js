aa5365cb567c939a000f8b205471d65ec7f1dc94
MV <mvcode00@gmail.com>
2021-03-20 20:57:05 +0100

    javadocs files

---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.openwebnet4j"},{"l":"org.openwebnet4j.bundle"},{"l":"org.openwebnet4j.communication"},{"l":"org.openwebnet4j.message"}]
